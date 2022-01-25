13bff1aecfbbb9add0e8cad27c3211fc69e86684
Kevin Kendle <1066413+KKendle@users.noreply.github.com>
2022-01-25 11:43:56 -0600

    basic data fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 
 function App() {
+  const apiKey = "d596f250cbded496840a7f64fe11522c"
+  const apiBaseUrl = 'api.openweathermap.org/data/2.5/weather'
+  const tempUnits = 'imperial'
+  const city = 'Saint Louis'
+  const state = 'MO'
+
+  fetch(`https://${apiBaseUrl}?q=${city}&${state}&units=${tempUnits}&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <h1>hello weather</h1>
     </div>
   );
 }
