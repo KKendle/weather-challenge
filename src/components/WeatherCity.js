9442c6c650c8384eeb2719b43f8540187e8d5bcd
Kevin Kendle <1066413+KKendle@users.noreply.github.com>
2022-01-25 16:54:46 -0600

    create multiple WeatherCity components from sample data

--- src/components/WeatherCity.js
+++ src/components/WeatherCity.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
 function WeatherCity(props) {
+  // location
+  // fetch(`https://${props.apiBaseUrl}?q=${props.data.city}&${props.data.state}&units=${props.tempUnits}&appid=${props.apiKey}`)
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     console.log(data)
+  //   })
+
   return (
     <article className="weather-city">
-      <h2 className="weather-city__city-name">${props.cityName}</h2>
-      <p className="weather-city__pressure">${props.pressure}</p>
-      <p className="weather-city__humidity">${props.humidity}</p>
-      <p className="weather-city__temperature">${props.temperature}</p>
+      <h2 className="weather-city__city-name">{props.data.city}</h2>
+      <p className="weather-city__pressure">{props.data.pressure}</p>
+      <p className="weather-city__humidity">{props.data.humidity}</p>
+      <p className="weather-city__temperature">{props.data.temperature}</p>
     </article>
   )
 }
---
+++ src/weatherData.js
@@ -0,0 +1,164 @@
+const weatherData = [
+  {
+    id: 1,
+    city: 'Saint Louis',
+    state: 'MO',
+    pressure: '15',
+    humidity: '50',
+    temperature: '23'
+  },
+  {
+    id: 2,
+    city: 'Saint Louis',
+    state: 'MO',
+    pressure: '15',
+    humidity: '50',
+    temperature: '23'
+  },
+  {
+    id: 3,
+    city: 'Saint Louis',
+    state: 'MO',
+    pressure: '15',
+    humidity: '50',
+    temperature: '23'
+  },
+  {
+    id: 4,
+    city: 'Saint Louis',
+    state: 'MO',
+    pressure: '15',
+    humidity: '50',
+    temperature: '23'
+  },
+  {
+    id: 5,
+    city: 'Saint Louis',
+    state: 'MO',
+    pressure: '15',
+    humidity: '50',
+    temperature: '23'
+  },
+  {
+    id: 6,
+    city: 'Saint Louis',
+    state: 'MO',
+    pressure: '15',
+    humidity: '50',
+    temperature: '23'
+  },
+  {
+    id: 7,
+    city: 'Saint Louis',
+    state: 'MO',
+    pressure: '15',
+    humidity: '50',
+    temperature: '23'
+  },
+  {
+    id: 8,
+    city: 'Saint Louis',
+    state: 'MO',
+    pressure: '15',
+    humidity: '50',
+    temperature: '23'
+  },
+  {
+    id: 9,
+    city: 'Saint Louis',
+    state: 'MO',
+    pressure: '15',
+    humidity: '50',
+    temperature: '23'
+  },
+  {
+    id: 10,
+    city: 'Saint Louis',
+    state: 'MO',
+    pressure: '15',
+    humidity: '50',
+    temperature: '23'
+  },
+  {
+    id: 11,
+    city: 'Saint Louis',
+    state: 'MO',
+    pressure: '15',
+    humidity: '50',
+    temperature: '23'
+  },
+  {
+    id: 12,
+    city: 'Saint Louis',
+    state: 'MO',
+    pressure: '15',
+    humidity: '50',
+    temperature: '23'
+  },
+  {
+    id: 13,
+    city: 'Saint Louis',
+    state: 'MO',
+    pressure: '15',
+    humidity: '50',
+    temperature: '23'
+  },
+  {
+    id: 14,
+    city: 'Saint Louis',
+    state: 'MO',
+    pressure: '15',
+    humidity: '50',
+    temperature: '23'
+  },
+  {
+    id: 15,
+    city: 'Saint Louis',
+    state: 'MO',
+    pressure: '15',
+    humidity: '50',
+    temperature: '23'
+  },
+  {
+    id: 16,
+    city: 'Saint Louis',
+    state: 'MO',
+    pressure: '15',
+    humidity: '50',
+    temperature: '23'
+  },
+  {
+    id: 17,
+    city: 'Saint Louis',
+    state: 'MO',
+    pressure: '15',
+    humidity: '50',
+    temperature: '23'
+  },
+  {
+    id: 18,
+    city: 'Saint Louis',
+    state: 'MO',
+    pressure: '15',
+    humidity: '50',
+    temperature: '23'
+  },
+  {
+    id: 19,
+    city: 'Saint Louis',
+    state: 'MO',
+    pressure: '15',
+    humidity: '50',
+    temperature: '23'
+  },
+  {
+    id: 20,
+    city: 'Saint Louis',
+    state: 'MO',
+    pressure: '15',
+    humidity: '50',
+    temperature: '23'
+  },
+]
+
+export default weatherData
