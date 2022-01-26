4c60a9f8b6a275e04225bae94c7d349fbb5bd626
Kevin Kendle <1066413+KKendle@users.noreply.github.com>
2022-01-25 20:53:45 -0600

    setup useState for fetching weather data

--- src/components/WeatherCity.js
+++ src/components/WeatherCity.js
@@ -1,19 +1,25 @@
 import React from 'react'
 
 function WeatherCity(props) {
-  // location
-  // fetch(`https://${props.apiBaseUrl}?q=${props.data.city}&${props.data.state}&units=${props.tempUnits}&appid=${props.apiKey}`)
-  //   .then(response => response.json())
-  //   .then(data => {
-  //     console.log(data)
-  //   })
+  // City Name, Pressure, Humidity, Temperature
+  let [cityPressure, setCityPressure] = React.useState(null)
+  let [cityHumidity, setCityHumidity] = React.useState(null)
+  let [cityTemperature, setCityTemperature] = React.useState(null)
+
+  fetch(`https://${props.apiBaseUrl}?q=${props.data.city}&${props.data.state}&units=${props.tempUnits}&appid=${props.apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      setCityPressure(data.main.pressure)
+      setCityHumidity(data.main.humidity)
+      setCityTemperature(data.main.temp)
+    })
 
   return (
     <article className="weather-city">
       <h2 className="weather-city__city-name">{props.data.city}</h2>
-      <p className="weather-city__pressure">{props.data.pressure}</p>
-      <p className="weather-city__humidity">{props.data.humidity}</p>
-      <p className="weather-city__temperature">{props.data.temperature}</p>
+      <p className="weather-city__pressure">{cityPressure}</p>
+      <p className="weather-city__humidity">{cityHumidity}</p>
+      <p className="weather-city__temperature">{cityTemperature}</p>
     </article>
   )
 }
